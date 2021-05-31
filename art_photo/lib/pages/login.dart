4b5cb427f063226454a2bc749ce747fccbbc8a4d
veronikawilliams1 <veronikawilliams@mail.ru>
2021-05-31 23:32:30 +0300

    ios style on login page

---
+++ art_photo/lib/pages/login.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        automaticallyImplyLeading: false,
+      ),
+      child: SafeArea(
+        child: ListView(
+          restorationId: 'text_field_demo_list_view',
+          padding: const EdgeInsets.all(16),
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: CupertinoTextField(
+                textInputAction: TextInputAction.next,
+                restorationId: 'email_address_text_field',
+                keyboardType: TextInputType.emailAddress,
+                clearButtonMode: OverlayVisibilityMode.editing,
+                autocorrect: false,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: CupertinoTextField(
+                textInputAction: TextInputAction.next,
+                restorationId: 'login_password_text_field',
+                clearButtonMode: OverlayVisibilityMode.editing,
+                obscureText: true,
+                autocorrect: false,
+              ),
+            ),
+            CupertinoTextField(
+              textInputAction: TextInputAction.done,
+              restorationId: 'pin_number_text_field',
+              prefix: const Icon(
+                CupertinoIcons.padlock_solid,
+                size: 28,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+              clearButtonMode: OverlayVisibilityMode.editing,
+              keyboardType: TextInputType.number,
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    width: 0,
+                    color: CupertinoColors.inactiveGray,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
